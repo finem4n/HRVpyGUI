77e9ba490007aee4cc551d7b92a62648f848870d
Konrad Marciniak <marciniakkonrad@outlook.com>
2025-04-29 01:34:46 +0200

    (fix): float precision warning

--- src/main_window.py
+++ src/main_window.py
@@ -307,7 +307,7 @@ class MainWindow(QMainWindow):
             tmp = hrv_all_renamed[col][0]
             if isinstance(tmp, float):
                 tmp = round(tmp, 2)
-                hrv_all_renamed[col][0] = tmp
+                hrv_all_renamed.loc[0, col] = tmp
 
         outputWindow = data_window.outputWidget(self.filename, hrv_all_renamed, detrended=self.detrended)
         outputWindow.exec()
